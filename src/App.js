34d6aad44ac73a7a526ce1d9c8f1cd1a94d48bfa
dbaigalmaa <dbaigalmaa@gmail.com>
2023-03-10 09:44:26 -0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.avif";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        Hello
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Bella</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return <div className="Dictonary">Hello from Dictionary</div>;
+}
